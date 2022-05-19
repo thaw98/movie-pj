1c31256387c1fbb613d3603f0d961fa418c359da
thaw98 <ppthaw1998@gmail.com>
2022-05-20 00:27:42 +0630

    commit

--- js/app.js
+++ js/app.js
@@ -14,12 +14,29 @@ var waypoint = new Waypoint({
   offset: "70%",
 });
 // Carousel
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-});
-$(".owl-carousel").owlCarousel({
-  items: 4,
-  lazyLoad: true,
-  loop: true,
-  margin: 10,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
 });
