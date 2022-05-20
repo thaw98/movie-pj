4931e14963e8ff88832ecef83fceda483e6a1cc7
thaw98 <ppthaw1998@gmail.com>
2022-05-20 22:29:27 +0630

    trending section almost complete

--- js/app.js
+++ js/app.js
@@ -17,9 +17,14 @@ var waypoint = new Waypoint({
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  autoplay: {
+    delay: 3400,
+    disableOnInteraction: false,
   },
   navigation: {
     nextEl: ".swiper-button-next",
